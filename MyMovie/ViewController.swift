870381b0f06b1fbe1b69671f83c4ce3ee0ac0c24
Sidney Silva <sidney_silva13@outlook.com>
2018-03-10 11:25:33 -0300

    Initial Commit

---
+++ MyMovie/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyMovie
+//
+//  Created by Sidney Silva on 10/03/2018.
+//  Copyright © 2018 Sakura Soft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
